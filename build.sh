0cb778194ab923e3be50f7f87671c7937e50035d
olusegun.odewole <olusegun.odewole@gunsel.com.tr>
2022-10-06 16:40:06 +0300

    added build

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
 
-poetry install
-
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate accounts
 python manage.py migrate 
